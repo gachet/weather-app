2fdbb51dec572c23e46ccc1e8c87e4d4edb0bb80
tdnvl <bummel1889@gmail.com>
2018-07-10 15:27:16 -0400

    Build URL and add API call

--- routes.py
+++ routes.py
@@ -1,16 +1,20 @@
 from flask import Flask, render_template, request, session, url_for, redirect
-
-api_key = open("api.txt", "r").readline()
-print(api_key)
-
+from urllib.request import urlopen
+import json
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-	return render_template("index.html")
+	api_key = str(open("api.txt", "r").read().splitlines())
+	elements = ["http://api.wunderground.com/api/",api_key[2:17],"/conditions/q/NY/Ithaca.json"]
+	url = "".join(elements)
+	json_obj = urlopen(url)
+	data = json.load(json_obj)
+	temperature = data['current_observation']['temperature_string']
+	return render_template("base.html", temperature=temperature)
 
 
 # Default port:
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
